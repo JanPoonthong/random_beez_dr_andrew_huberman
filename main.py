b27278e2733487eba116e213b93cda89e71b3001
Jan Poonthong <janpoonthong628@gmail.com>
2023-08-03 10:13:06 +0700

    wip: just generate numbers without gaps

--- main.py
+++ main.py
@@ -36,36 +36,32 @@ def main():
     total_amount_of_beez = 30
 
     i = 0
+    count = 1
+    random_int = 0
     while i < total_amount_of_beez:
-        random_int = random.randint(0, minute * 60)
+        if random_int >= minute * 60:
+            break
+        random_int = random.randint(random_int, random_int + 10)
 
         if len(list_of_random) > 0:
-            while (
-                abs(random_int - list_of_random[i - 1]) <= gap
-                or random_int < list_of_random[i - 1]
-            ):
+            while random_int < list_of_random[i - 1]:
                 random_int = random.randint(0, minute * 60)
-                if (
-                    abs(random_int - list_of_random[i - 1]) > gap
-                    and random_int > list_of_random[i - 1]
-                ):
+                if random_int > list_of_random[i - 1]:
                     if random_int not in list_of_random:
                         list_of_random.append(random_int)
+                        count += 1
                         i += 1
-                        list_of_random = sorted(list_of_random)
                         break
 
             if random_int not in list_of_random:
                 list_of_random.append(random_int)
-                list_of_random = sorted(list_of_random)
                 i += 1
         else:
             list_of_random.append(random_int)
+            count += 1
             i += 1
 
-    for i in list_of_random:
-        print(i/60, end=",")
-    list_of_random = sorted(list_of_random)
+    print(list_of_random)
     play_random_beez(minute, list_of_random)
 
 
